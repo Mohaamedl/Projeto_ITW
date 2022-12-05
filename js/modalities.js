8dde19645c24d4b61c6d6bdad55644b8db98fbce
Mohaamedl <mohamedhadodo33@gmail.com>
2022-12-05 23:30:12 +0000

    modalities v2

--- js/modalities.js
+++ js/modalities.js
@@ -2,7 +2,7 @@ var vm = function () {
     console.log('ViewModel initiated...');
     //---Variáveis locais
     var self = this;
-    self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/Modalities');
+    self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/Modalities/');
     //self.baseUri = ko.observable('http://localhost:62595/api/drivers');
     self.displayName = 'Olympic Modalities List';
     self.error = ko.observable('');
