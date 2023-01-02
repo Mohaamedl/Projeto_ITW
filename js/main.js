da080e61e382bfeffdcc04a81e46c30bd25694fc
Mohaamedl <mohamedhadodo33@gmail.com>
2023-01-02 19:56:33 +0000

    final

--- js/main.js
+++ js/main.js
@@ -77,15 +77,15 @@ function moveToSelected(element) {
   $('#next').click(function() {
     moveToSelected('next');
   });
-  let items = document.querySelectorAll('.carousel .carousel-item')
+  let itemsc = document.querySelectorAll('.carousel .carousel-item')
 
-		items.forEach((el) => {
+		itemsc.forEach((el) => {
 			const minPerSlide = 4
 			let next = el.nextElementSibling
 			for (var i=1; i<minPerSlide; i++) {
 				if (!next) {
             // wrap carousel by using first child
-            next = items[0]
+            next = itemsc[0]
         }
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
