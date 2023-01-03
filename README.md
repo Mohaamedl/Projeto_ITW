c41b18232d17a33851570523a6739018c34bc9a3
Mohaamedl <mohamedhadodo33@gmail.com>
2023-01-03 13:23:01 +0000

    acabado

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Projeto_ITW
+# Projeto_ITW 
+Website developed by Mohamed Haddadi(n.107193) and Rodrigo Abreu (n. 113626)
+
+#Alterações
+1 - As tabelas presentes em Competition details e Modality details foram postas em accordion
+2 - As tabela em Country details e Game details foram colocadas em nav tabs
--- js/main.js
+++ js/main.js
@@ -77,6 +77,91 @@ function moveToSelected(element) {
   $('#next').click(function() {
     moveToSelected('next');
   });
+  (function ($) {
+
+  "use strict";
+
+
+  // Form
+  var contactForm = function () {
+      if ($('#contactForm').length > 0) {
+          $("#contactForm").validate({
+              rules: {
+                  name: {required:true, minlength: 2},
+                  subject: {required:true, minlength: 3},
+                  email: {
+                      required: true,
+                      email: true
+                  },
+                  message: {
+                      required: true,
+                      minlength: 10
+                  }
+              },
+              messages: {
+                  name: "Please enter your name",
+                  subject: "Please enter your subject",
+                  email: "Please enter a valid email address",
+                  message: "Please enter a message"
+              },
+              /* submit por ajax */
+
+              submitHandler: function (form) {
+                  var $submit = $('.submitting'),
+                      waitText = 'Submitting...';
+
+                  $.ajax({
+                      type: "POST",
+                      url: "php/sendEmail.php",
+                      data: $(form).serialize(),
+
+                      beforeSend: function () {
+                          $submit.css('display', 'block').text(waitText);
+                      },
+                      success: function (msg) {
+                          if (msg == 'OK') {
+                              $('#form-message-warning').hide();
+                              setTimeout(function () {
+                                  $('#contactForm').fadeIn();
+                              }, 1000);
+                              setTimeout(function () {
+                                  $('#form-message-success').fadeIn();
+                              }, 1400);
+
+                              setTimeout(function () {
+                                  $('#form-message-success').fadeOut();
+                              }, 8000);
+
+                              setTimeout(function () {
+                                  $submit.css('display', 'none').text(waitText);
+                              }, 1400);
+
+                              setTimeout(function () {
+                                  $('#contactForm').each(function () {
+                                      this.reset();
+                                  });
+                              }, 1400);
+
+                          } else {
+                              $('#form-message-warning').html(msg);
+                              $('#form-message-warning').fadeIn();
+                              $submit.css('display', 'none');
+                          }
+                      },
+                      error: function () {
+                          $('#form-message-warning').html("Something went wrong. Please try again.");
+                          $('#form-message-warning').fadeIn();
+                          $submit.css('display', 'none');
+                      }
+                  });
+              } // end submitHandler
+
+          });
+      }
+  };
+  contactForm();
+
+})(jQuery);
   let itemsc = document.querySelectorAll('.carousel .carousel-item')
 
 		itemsc.forEach((el) => {
